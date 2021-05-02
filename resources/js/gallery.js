329827962a366fcdef7a965ac63e8bdc284a959d
mumblebaj <bernardmumble@gmail.com>
2021-05-02 18:54:32 +0200

    add photo gallery

---
+++ resources/js/gallery.js
@@ -0,0 +1,80 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+// Helper Function to St Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // Run functions for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        // Create <img> for Photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded());
+        // Put  <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+let count = 5;
+const apiKey = 'zaAAjDia1KE0I8-_ocG_CJfbmh0oVhadoFItOAO46ww';
+let apiUrl = `https://github.com/mumblebaj/mumble-family/tree/main/resources/gallery`;
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        count = 30;
+        apiUrl = `https://github.com/mumblebaj/mumble-family/tree/main/resources/gallery`;
+    }
+}
+
+// Get photos from Unsplah API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+        
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+// On Load
+getPhotos();
