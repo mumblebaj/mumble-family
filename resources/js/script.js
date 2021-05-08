2dc826c081d37ca13dde9ddf5eb25604d9a82ead
mumblebaj <bernardmumble@gmail.com>
2021-05-08 21:29:18 +0200

    update gm pages, removed unused code

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,3 +4,4 @@ AOS.init({
     once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     });
+
